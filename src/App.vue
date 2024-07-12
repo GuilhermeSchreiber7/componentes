87236be7d1efbba77e00969f5f8a2b0c90f95d84
GuilhermeSchreiber7 <guilhermeschreiber2007@gmail.com>
2024-07-12 15:12:44 -0300

    aula components

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+    import ExpandBox from './components/ExpandBox.vue'
+</script>
+
+<template>
+  <h1>Componentes</h1>
+  <hr>
+  <ExpandBox title="Bola" content="Bolas"/>
+  <expand-box title="Quadrado" content="Quadrados"/>
+</template>
+
+<style scoped>
+
+</style>
